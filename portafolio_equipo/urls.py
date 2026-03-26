c845c13598403939bc93e72e26c5dbe23dfc9c67
JuanMontezuma <juanjomontezuma@gmail.com>
2026-03-26 15:48:05 -0500

    feat: agregar pagina de inicio con menu de navegacion

--- portafolio_equipo/urls.py
+++ portafolio_equipo/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import home  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
     path('juan/', include('cv_juan.urls')),
     path('miguel/', include('cv_miguel.urls')),
     path('sebas/', include('cv_sebas.urls')),
